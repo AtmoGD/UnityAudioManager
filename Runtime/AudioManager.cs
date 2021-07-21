94c1342e74f3dfcd4aa8389d3fab83f7fc6296fe
Dennis Hawran <56675253+AionixX@users.noreply.github.com>
2021-07-21 10:37:44 +0200

    Update

--- Runtime/AudioManager.cs
+++ Runtime/AudioManager.cs
@@ -5,46 +5,42 @@ using System.Collections.Generic;
 /*
 THANK YOU BRACKEYS
 */
-
-namespace Basic
+public class AudioManager : MonoBehaviour
 {
-    public class AudioManager : MonoBehaviour
+    public static AudioManager instance;
+    public List<Sound> sounds = new List<Sound>();
+
+    void Awake()
     {
-        public static AudioManager instance;
-        public List<Sound> sounds = new List<Sound>();
-
-        void Awake()
+        if (AudioManager.instance != null)
         {
-            if (AudioManager.instance != null)
-            {
-                Destroy(gameObject);
-                Debug.LogError("There can only be one AudioManager");
-                return;
-            }
-
-            DontDestroyOnLoad(gameObject);
-
-            AudioManager.instance = this;
-
-            foreach (Sound sound in sounds)
-            {
-                sound.source = gameObject.AddComponent<AudioSource>();
-                sound.source.clip = sound.clip;
-                sound.source.volume = sound.volume;
-                sound.source.pitch = sound.pitch;
-                sound.source.loop = sound.loop;
-                if(sound.playOnAwake)
-                    sound.source.Play();
-            }
+            Destroy(gameObject);
+            Debug.LogError("There can only be one AudioManager");
+            return;
         }
 
-        public void Play(string _name)
+        DontDestroyOnLoad(gameObject);
+
+        AudioManager.instance = this;
+
+        foreach (Sound sound in sounds)
         {
-            Sound sound = sounds.Find(sounds => sounds.name == _name);
-            if (sound != null)
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+            if (sound.playOnAwake)
                 sound.source.Play();
-            else
-                Debug.LogError("No sound with name " + _name + " exists.");
         }
+    }
+
+    public void Play(string _name)
+    {
+        Sound sound = sounds.Find(sounds => sounds.name == _name);
+        if (sound != null)
+            sound.source.Play();
+        else
+            Debug.LogError("No sound with name " + _name + " exists.");
     }
 }
--- Runtime/Sound.cs
+++ Runtime/Sound.cs
@@ -1,17 +1,14 @@
 using UnityEngine.Audio;
 using UnityEngine;
 
-namespace Basic
+[System.Serializable]
+public class Sound
 {
-    [System.Serializable]
-    public class Sound
-    {
-        public string name;
-        public AudioClip clip;
-        [Range(0.0f, 1.0f)] public float volume = 1f;
-        [Range(0.1f, 3.0f)] public float pitch = 1f;
-        public bool loop = false;
-        public bool playOnAwake = true;
-        [HideInInspector] public AudioSource source;
-    }
+    public string name;
+    public AudioClip clip;
+    [Range(0.0f, 1.0f)] public float volume = 1f;
+    [Range(0.1f, 3.0f)] public float pitch = 1f;
+    public bool loop = false;
+    public bool playOnAwake = true;
+    [HideInInspector] public AudioSource source;
 }
